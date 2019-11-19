0de18dbcecb1c87ed93af4923d683ea2af191f3a
Jacky Lau <jackylau0126@gmail.com>
2019-11-19 03:43:14 +0000

    implment validations, method for validations and changed testing for first/last name to name instead

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject {describe_class.new(first_name: 'Sun', last_name: 'Sun', email: 'sun@gmail.com', password: '128', password_confirmation: '128')}
-
+  subject {described_class.new(:name => 'Sun', :email => 'sun@gmail.com', :password => "128", :password_confirmation => "128")}
   describe 'Validation' do
   
     it 'saves successfully when all four fields are set' do
@@ -16,14 +15,8 @@ RSpec.describe User, type: :model do
       expect(subject.errors).not_to be_empty
     end
 
-    it 'fails to save when first name is not set' do
-      subject.first_name = nil
-      subject.valid?
-      expect(subject.errors).not_to be_empty
-    end
-
-    it 'fails to save when last name is not set' do
-      subject.last_name = nil
+    it 'fails to save when name is not set' do
+      subject.name = nil
       subject.valid?
       expect(subject.errors).not_to be_empty
     end
@@ -40,14 +33,14 @@ RSpec.describe User, type: :model do
       expect(subject.errors).not_to be_empty
     end
 
-    it 'fails to save when password and password_confirmation does not match' do
+    it 'fails to save when password and password_confirmation is not identical' do
       subject.password_confirmation = '000'
       subject.valid?
       expect(subject.errors).not_to be_empty
     end
     
     it 'fails to save when email is not unique (not case sensitive)' do
-      User.create(:first_name => 'Sun', :last_name => 'Sun', :email => 'sun@gmail.com', :password => "128", :password_confirmation => "128", :password_digest => "111")
+      User.create(:name => 'Sun', :email => 'sun@gmail.com', :password => "128", :password_confirmation => "128", :password_digest => "111")
       subject.valid?
       expect(subject.errors).not_to be_empty
     end
@@ -60,27 +53,27 @@ RSpec.describe User, type: :model do
     end
 
   end
-
+  
   describe '.authenticate_with_credentials' do
-    it 'returns user if succesfully authenticated' do
+    it 'allows user to login if succesfully authenticated' do
       subject.save
       user = User.authenticate_with_credentials('sun@gmail.com', '128')
       expect(subject).to be == user
     end
 
-    it 'returns nil if not successfully authenticated' do
+    it 'fails to login if unsuccessfully authenticated' do
       subject.save
       user = User.authenticate_with_credentials('sun@gmail.com', 'sss')
       expect(user).to be == nil
     end
 
-    it 'authenticates and users if user type white space before / after email' do
+    it 'authenticates users even if white space is typed before and/or after email' do
       subject.save
       user = User.authenticate_with_credentials(' sun@gmail.com ', '128')
       expect(subject).to be == user
     end
 
-    it 'authenticates and users if users type lower and upper case in email' do
+    it 'allows emails to be non-case sensitive and authenticates the user to login' do
       subject.save
       user = User.authenticate_with_credentials('sUn@gMaIl.com', '128')
       expect(subject).to be == user
